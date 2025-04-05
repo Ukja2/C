0718b2f495e608ff2e50a3bdaf7479ea2a6be331
Ukja2 <jaewooklee739@gmail.com>
2025-04-05 20:32:27 +0900

    증감 연산자로 연산 2

---
+++ Chapter4/Ex4-6/main.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main() {
+
+	int a = 5, b = 5;
+	int re1 = 0, re2 = 0;
+
+	printf("초기상태 a: %d, b: %d\n", a, b);
+	printf("초기상태 re1: %d, re2: %d\n", re1, re2);
+
+	++a; //전위연산
+	re1 = a;
+	re2 = b++;
+
+	printf("연산 후 결과 a : %d, b : %d\n", a, b);
+	printf("연산 후 결과 re1 : %d, re2 : %d\n", re1, re2);
+
+	return 0;
+}
