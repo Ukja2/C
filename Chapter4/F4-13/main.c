03b3853225a0123bcbbbd2ec2e67e9d6b1487b93
Ukja2 <jaewooklee739@gmail.com>
2025-04-05 20:50:55 +0900

    비트 논리 연산자 사용

---
+++ Chapter4/F4-13/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a = 30;
+    // 왼쪽으로 1비트씩 이동할 때마다 값이 2배가 됨
+    printf("a << 1 = %d\n", a << 1);
+    printf("a << 2 = %d\n", a << 2);
+    printf("a << 3 = %d\n", a << 3);
+    printf("a << 4 = %d\n", a << 4);
+    return 0;
+}
