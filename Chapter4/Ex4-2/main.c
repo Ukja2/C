68b45a5ff2b04a0e72af8eebcfe7d71d686758c7
Ukja2 <jaewooklee739@gmail.com>
2025-04-05 20:20:36 +0900

    대입 연산잔

---
+++ Chapter4/Ex4-2/main.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main() {
+	int a = 10;
+	printf("a = %d\n", a);
+
+	int x, y, z;
+	x = y = z = 20;
+	printf("x = %d, y= %d, z = %d\n", x, y, z);
+
+	int num = 5, score = 3;
+	num += 3;
+	score *= 2;
+	printf("num = %d\n", num);
+	printf("score = %d\n", score);
+
+	return 0;
+
+}
