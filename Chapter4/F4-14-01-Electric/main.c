84e3579e9c62cb7250a4bac8b912413ab9883018
Ukja2 <jaewooklee739@gmail.com>
2025-04-05 21:26:23 +0900

    전구 상태 변경

--- Chapter4/F4-14-01-Electric/main.c
+++ Chapter4/F4-14-01-Electric/main.c
@@ -1,21 +1,16 @@
+#pragma warning(disable:4996)
 #include <stdio.h>
 
-int main() {
-    int x = 10;          // 정수 변수
-    int* p;               // 정수 포인터 선언
+int main()
+{
+    int bulbState;
 
-    p = &x;              // 변수 x의 메모리 주소를 반환한 후 p에 대입
+    printf("전구의 현재 상태를 입력하세요 (-1 또는 1): ");
+    scanf("%d", &bulbState);
 
-    printf("x의 값: %d\n", x);
-    printf("x의 주소: %p\n", &x);
-    printf("p가 가리키는 주소: %p\n", p);
-    printf("p가 가리키는 값 (참조): %d\n", *p);
+    bulbState = ~bulbState + 1;
 
-    // p를 통해 x의 값을 변경
-    *p = 20;              // 포인터 p가 가리키는 메모리 위치의 값 반환
-
-
-    printf("변경된 x의 값: %d\n", x);
+    printf("변경된 전구의 상태: %d\n", bulbState);
 
     return 0;
 }
