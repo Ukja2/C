61025254d3ebe4ebbc56b2e6c6164a5d59df5a48
Ukja2 <jaewooklee739@gmail.com>
2025-04-07 17:16:39 +0900

    단순 if 문을 사용하여 정수 판단하기

---
+++ Chapter5/Ex5-02/main.c
@@ -0,0 +1,25 @@
+#pragma warning(disable:4996)
+#include<stdio.h>
+
+int main() {
+
+	int num;
+	printf("정수를 입력하세요: ");
+	scanf("%d", &num);
+
+	if (num > 0) {
+		printf("%d는 양수입니다.\n", num);
+	}
+
+	if (num < 0) {
+		printf("%d는 음수입니다.\n", num);
+	}
+
+	if (num == 0) {
+		printf("0입니다.\n", num);
+	}
+
+	return 0;
+
+
+}
