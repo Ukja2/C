0f3f212e61e97abfd1e855f9cea89b56b9cba7f5
Ukja2 <jaewooklee739@gmail.com>
2025-04-07 17:50:50 +0900

    if else문으로 알파벳 문자 변경하기

--- Chapter5/Ex5-04/main.c
+++ Chapter5/Ex5-04/main.c
@@ -1,21 +1,39 @@
 #pragma warning(disable:4996)
 #include<stdio.h>
+#include<ctype.h>
 
 int main() {
 
 	char ch;
-	printf("알파벳을 입력하세요: ");
+	//printf("알파벳을 입력하세요: ");
 
+	//scanf(" %c", &ch);
+
+	//if (ch >= 'a' && ch <= 'z') {
+	//	//ch = (ch - 'a' + 3) % 26 + 'a';
+	//	ch = ch - 32; 대소문자변환
+	//}
+	//else {
+	//	//ch = (ch - 'A' + 3) % 26 + 'A';
+	//	ch = ch + 32; 대소문자 변환
+	//}
+	//printf("변경된 문자: %c\n", ch);
+
+	
+
+	while ((getchar()) != '\n'); // 이전 입력에서 남은 개행 문자 제거(필요시)
+	
+	printf("알파벳을 입력하세요: ");
 	scanf(" %c", &ch);
 
-	if (ch >= 'a' && ch <= 'z') {
+	if (islower(ch)) {
 		ch = (ch - 'a' + 3) % 26 + 'a';
 	}
-	else {
+	else if (isupper(ch)) {
 		ch = (ch - 'A' + 3) % 26 + 'A';
 	}
-
-	printf("변경된 문자: %c\n", ch);
 	
+	printf("변경된 문자: %c\n", ch);
+
 	return 0;
 }
