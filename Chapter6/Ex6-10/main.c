dd73aaa8a3d6f63dbeb301a9e383477f4231ed22
Ukja2 <jaewooklee739@gmail.com>
2025-04-17 14:36:14 +0900

    다중 for 문을 사용하여 구구단 출력하기

---
+++ Chapter6/Ex6-10/main.c
@@ -0,0 +1,15 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+
+int main() {
+
+	int i, j;
+
+	for (i = 1; i <= 9; i++) {
+		for (j = 1; j <= 9; j++) {
+			printf("%d * %d = %2d \n", i, j, i * j);
+		}
+	}
+
+	return 0;
+}
