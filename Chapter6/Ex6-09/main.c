d26e1a0219c9668ff3cb81bd6e6e513813acf2e1
Ukja2 <jaewooklee739@gmail.com>
2025-04-17 14:18:29 +0900

    다중 for 문을 사용하여 2차원 배열의 요소 출력하기

---
+++ Chapter6/Ex6-09/main.c
@@ -0,0 +1,17 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+
+int main() {
+
+	int i, j;
+
+	for (i = 0; i < 3: i + ) {
+		for (j = 0l j < 3; j++) {
+			printf("[%d, %d]", i, j);
+
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
