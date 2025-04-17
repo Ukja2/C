4de639781f91b95071658be80a407284c3101a25
Ukja2 <jaewooklee739@gmail.com>
2025-04-17 13:44:05 +0900

    while 문을 사용하여 여러 번 입력된 정수의 합 출력하기

--- Chapter6/Ex6-03/main.c
+++ Chapter6/Ex6-03/main.c
@@ -4,12 +4,36 @@
 int main() {
 	int n = 0, sum = 0;
 
-	printf("숫자 입력(종료 : ctrl + z) : \n");
+	//printf("숫자 입력(종료 : ctrl + z) : \n");
 
-	while (scanf("%d", &n) != EOF) {
+	//while (scanf("%d", &n) != EOF) {
+	//	sum = sum + n;
+	//}
+	//printf("\n 합게 : %d \n", sum);
+
+
+	printf("숫자 입력(종료:0): ");
+	
+	//do {
+	//	scanf("%d", &n);
+	//	sum = sum + n;
+	//	
+	//} while (n != 0);
+
+
+
+	while (1) {
+		scanf("%d", &n);
 		sum = sum + n;
+
+		if (n == 0) {
+			break;
+		}
+
 	}
-	printf("\n 합게 : %d \n", sum);
+		printf(" \n 합계 : %d\n", sum);
+
+
 	return 0;
 }
 
