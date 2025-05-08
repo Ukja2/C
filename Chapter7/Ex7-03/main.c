af4bb8c7fa542d4612f14713cdfc481b48eb706d
Ukja2 <jaewooklee739@gmail.com>
2025-05-08 14:36:31 +0900

    매개변수와 반환값이 없는 함수 사용하기

--- Chapter7/Ex7-03/main.c
+++ Chapter7/Ex7-03/main.c
@@ -1,14 +1,24 @@
 #include<stdio.h>
+void function1();
+void function2();
+
+int main() {
+
+	function1();
+	int a = 5, b = 7;
+	function2(a, b);
+
+
+	return;
+}
+
+void function2(int x, int y) {
+	printf("매개변수 ㅇ 반환값  x\n");
+	printf("두 개의 매개변수 : %d. %d\n", x, y);
+}
 
 void function1() {
 	printf("매개변수도 없고 반환 값도 없는 함수");
 	printf("매개변수 : 없음\n");
 	printf("반환값 없음 : void\n");
-}
-
-int main() {
-
-	function1();
-
-	return;
 }
