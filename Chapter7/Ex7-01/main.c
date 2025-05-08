cab5ff68f27fab9051526605f4d1d5dd89b9c6cb
Ukja2 <jaewooklee739@gmail.com>
2025-05-08 13:51:50 +0900

    사용자 정의 함수를 사용하여 삼각형의 넓이 계산하기

---
+++ Chapter7/Ex7-01/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// 사용자 정의 함수 선언
+double triangleArea(double base, double height);
+
+// 함수 정의
+double triangleArea(double base, double height)
+{
+    return (base * height) / 2.0;  // 함수의 반환값
+}
+
+int main()
+{
+    double base = 10.0, height = 5.0;
+    double area = 0.0;
+
+    // triangleArea() 함수를 호출하여 base와 height의 값을 전달하고,
+    // 반환되는 삼각형의 넓이를 변수 area에 저장
+    area = triangleArea(base, height);
+
+    printf("삼각형의 밑변이 %.2lf, 높이가 %.2lf일 때\n", base, height);
+    printf("삼각형의 넓이는 %.2lf입니다.\n", area);
+
+    return 0;
+}
