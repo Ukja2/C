07bdfafa20231ce1d7e62b22a96b3ad9a624908c
Ukja2 <jaewooklee739@gmail.com>
2025-05-26 17:01:56 +0900

    포인터 변수 선언하고 초기화하기

--- Chapter10/Ex10-02/main.c
+++ Chapter10/Ex10-02/main.c
@@ -3,8 +3,10 @@
 int main() {
 
 	int value = 125;
-	int* ptr;
-	ptr = value;
+	int *ptr;
+
+	ptr = &value; // ptr은 value의 주소를 가리킴
+	// 네비에서 우리집의 개념 = (별 떼고 쓰는)ptr = 주소 저장
 
 	printf("value의 값: %d\n", value);
 	printf("value의 주소: %p\n", &value);
