e7f23f23798d5ff7774d441f3098613bf4cda8a3
Ukja2 <jaewooklee739@gmail.com>
2025-05-26 17:37:58 +0900

    포인터 연산 사용하기

---
+++ Chapter10/Ex10-05/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 10;
+    int* p;
+
+    p = &i;  // p에 i의 주소 저장
+
+    printf("p = %p \n", p);
+    printf("(*p)++ = %d \n", (*p)++);  // 후위 증가 연산으로 i의 값 출력 후 1 증가
+    printf("*p++ = %d \n", *p++);     // p가 가리키는 값 출력 후 포인터 p 증가
+    printf("p++ = %p \n", p++);       // 증가된 p의 주소 출력
+
+    return 0;
+}
