33caf081eaad4f2744d51eaaf3a92f6281017984
Ukja2 <jaewooklee739@gmail.com>
2025-05-26 17:18:49 +0900

    포인터 변수 메모리 할당 바이트 확인하기

---
+++ Chapter10/Ex10-04/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i = 10;
+    double d = 3.4;
+    int* pi;
+    double* pd;
+
+    printf("변수 pi에 할당된 바이트: %d(byte)\n", sizeof(pi));
+    printf("변수 pd에 할당된 바이트: %d(byte)\n", sizeof(pd));
+
+    return 0;
+}
