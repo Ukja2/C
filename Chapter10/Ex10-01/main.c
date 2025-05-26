74068fea5854fc49f72c1694d8e5569d06e67d6e
Ukja2 <jaewooklee739@gmail.com>
2025-05-26 16:49:21 +0900

    & 연산자로 변수의 메모리 주소 확인하기

---
+++ Chapter10/Ex10-01/main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+
+	char x = 'h';
+	int y = 100;
+	float z = 2.5f;
+
+	printf("x狼 皋葛府 林家 : %p\n", &x);
+	printf("y狼 皋葛府 林家 : %p\n", &y);
+	printf("z狼 皋葛府 林家 : %p\n", &z);
+
+	return 0;
+}
