08ac6c537afc090fab31a4bcefd98ff550b33e18
Ukja2 <jaewooklee739@gmail.com>
2025-06-02 16:40:57 +0900

    문자열 초기화하고 출력

---
+++ Chapter11/Ex11-01/main.c
@@ -0,0 +1,20 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+#include <string.h>  // strcpy() 함수 사용을 위한 헤더
+
+int main()
+{
+    char str1[] = "c language!";                     // 문자열 리터럴로 초기화
+    char str2[12] = { 'c',' ','l','a','n','g','u','a','g','e','!','\0' }; // 문자 배열 초기화
+    char str3[12] = "c language!";                   // 배열 선언 및 문자열 리터럴 초기화
+    char str4[12];                                   // 크기 12인 빈 문자 배열 선언
+
+    strcpy(str4, "c language!");                     // str4에 문자열 복사
+
+    printf("str1[] = %s \n", str1);
+    printf("str2[12] = %s \n", str2);
+    printf("str3[12] = %s \n", str3);
+    printf("str4[12] = %s \n", str4);
+
+    return 0;
+}
